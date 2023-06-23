2fea74b25d664c0980d0c7897c6e31ca6a80184c
medhavi <medhavisharma11apr@gmail.com>
2023-06-23 18:41:38 +0530

    new commit

--- flutter_application_1/lib/page_4.dart
+++ flutter_application_1/lib/page_4.dart
@@ -1,5 +1,7 @@
+//import 'dart:js_util';
+//import 'dart:math';
+
 import 'dart:js_util';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -10,16 +12,35 @@ class MyPage4 extends StatefulWidget {
 
 class _MyApp4 extends State<MyPage4> {
   final mycontroller = TextEditingController();
-  final List<String> todos = <String>['home', 'shop', 'exam', 'window'];
-  int counter = 0;
+  final mycontroller2 = TextEditingController();
+  final List<String> todos = <String>[];
+  String text = ' ';
+  bool isChecked = false;
+
+  //int counter = 0;
 
   @override
   Widget build(BuildContext context) {
+    // get color function
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
     return Scaffold(
+      // appbar build
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 19, 139, 238),
         title: Text('Flutter Todo List'),
       ),
+      // body of project
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -29,6 +50,7 @@ class _MyApp4 extends State<MyPage4> {
                   bottom: BorderSide(color: Color.fromARGB(255, 19, 139, 238)),
                 ),
               ),
+              // textfield and controller
               child: TextField(
                 controller: mycontroller,
               ),
@@ -38,26 +60,92 @@ class _MyApp4 extends State<MyPage4> {
             ),
             Column(
               children: [
+                // add button for adding from textfield
                 TextButton(
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all<Color>(
                           Colors.purple.shade100),
                     ),
                     onPressed: () {
-                      counter += 1;
+                      todos.add(mycontroller.text.toUpperCase());
+                      mycontroller.clear();
+                      setState(() {});
                     },
                     child: Text(
                         style: TextStyle(color: Colors.black), 'Add Todo')),
                 SizedBox(
                   height: 10,
                 ),
+                // here starts the container for list tile
                 Container(
                   height: 1000,
                   child: ListView.builder(
                       shrinkWrap: true,
-                      itemCount: counter,
-                      itemBuilder: (BuildContext context, int counter) {
-                        return Text('ghhhhgg');
+                      itemCount: todos.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: EdgeInsets.all(4),
+                          child: ListTile(
+                            leading: GestureDetector(
+                                onDoubleTap: () {
+                                  // edit option
+                                  mycontroller2.text = todos[index];
+                                  // dialouge box
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) => SimpleDialog(
+                                            children: [
+                                              TextField(
+                                                // text field input of dialouge box for editing purpose
+                                                controller: mycontroller2,
+                                                // onChanged: (value) {
+                                                //   setState(() {
+                                                //     text = value;
+                                                //   });
+                                                // },
+                                                // decoration: InputDecoration(
+                                                //   hintText: todos[index]),
+                                              ),
+                                              ElevatedButton(
+                                                  // this is elevate buttn of dialouge box
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      todos[index] =
+                                                          mycontroller2.text;
+                                                    });
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text('Update'))
+                                            ],
+                                          ));
+                                },
+                                child: Icon(Icons.edit)),
+                            title: Text('  ${todos[index]} '),
+                            tileColor: Colors.grey.shade300,
+                            // last part of tile
+                            trailing: Row(
+                              children: [
+                                Container(
+                                  width: 50,
+                                  child: Checkbox(
+                                      value: isChecked,
+                                      onChanged: (bool? value) {
+                                        setState(() {
+                                          isChecked = value!;
+                                        });
+                                      }),
+                                ),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        todos.removeAt(index);
+                                      });
+                                    },
+                                    child: Icon(Icons.delete)),
+                              ],
+                            ),
+                          ),
+                        );
                       }),
                 )
               ],
